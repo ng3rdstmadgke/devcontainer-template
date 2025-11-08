ad5ed7c507e360eb024bb7b63447748856067e6b
ktamido <ng3rdstmadgke@gmail.com>
2025-11-08 13:45:31 +0900

    claude 追加

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mkdir -p ~/.ssh
+mkdir -p ~/.aws
+mkdir -p ~/.devcontainer-template/.kube
+mkdir -p ~/.devcontainer-template/.config/helm
+mkdir -p ~/.devcontainer-template/.claude
 
 DOCKER_NETWORK=br-devcontainer-template-${USER}
 NETWORK_EXISTS=$(docker network ls --filter name=$DOCKER_NETWORK --format '{{.Name}}')
