ad1154f8176642684e5f7ca52314be8a60e805fe
ktamido <ng3rdstmadgke@gmail.com>
2026-02-02 23:26:01 +0900

    いろいろ修正

---
+++ .devcontainer/conf/common/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRIPT_DIR=$1
+
+cp $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-cp ${PROJECT_DIR}/.devcontainer/etc/.tmux.conf ~/.tmux.conf
-
 cat <<EOF >> ~/.bashrc
 
 source ${PROJECT_DIR}/.devcontainer/.bashrc_private
